8ace98986461a1207f325a1dfff7edf6c54e5c92
Sam Sheets <sam.sheets94@gmail.com>
2021-03-28 11:52:44 -0600

    inline styling of Square component

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+//inline style: JavaScript object
+const style = {
+    background: 'lightblue',
+    border: '2px solid darkblue',
+    fontSize: '30px',
+    fontWeight: '800',
+    cursor: 'pointer',
+    outline: 'none'
+}
+
 function Square(props) {
     const {onClick, value} = props
     return (
-        <button onClick={onClick}>
+        <button style={style} onClick={onClick}>
             {value}
         </button>
     )
